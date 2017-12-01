84238a56292d20fe54cd68a8dde1127c55843f13
fjmtkut <190367v@ugs.kochi-tech.ac.jp>
2017-12-01 12:23:19 +0900

    compilerExprの内容を追加

--- TinyPiE/src/Compiler.java
+++ TinyPiE/src/Compiler.java
@@ -9,7 +9,38 @@ import parser.TinyPiEParser;
 
 public class Compiler extends CompilerBase {
 	void compileExpr(ASTNode ndx, Environment env) {
-		throw new Error("not implemented");
+		if (ndx instanceof ASTBinaryExprNode) {
+			ASTBinaryExprNode nd = (ASTBinaryExprNode) ndx;
+			compileExpr(nd.lhs, env);
+			emitPUSH(REG_R1);
+			emitRR("mov", REG_R1, REG_DST);
+			compileExpr(nd.rhs, env);
+			if (nd.op.equals("+"))
+				emitRRR("add", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("-"))
+				emitRRR("sub", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("*"))
+				emitRRR("mul", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("/"))
+				emitRRR("udiv", REG_DST, REG_R1, REG_DST);
+			else
+				throw new Error("Unknown operator: "+nd.op);
+			emitPOP(REG_R1);
+		} else if (ndx instanceof ASTNumberNode) {
+			ASTNumberNode nd = (ASTNumberNode) ndx;
+		} else if (ndx instanceof ASTVarRefNode) {
+			ASTVarRefNode nd = (ASTVarRefNode) ndx;
+			Variable var = env.lookup(nd.varName);
+			if (var == null)
+				throw new Error("Undefined variable: "+nd.varName);
+			if (var instanceof GlobalVariable) {
+				GlobalVariable globalVar = (GlobalVariable) var;
+				emitLDC(REG_DST, globalVar.getLabel());
+				emitLDR(REG_DST, REG_DST, 0);
+			} else
+				throw new Error("not a blobal variable: "+nd.varName);
+		} else
+			throw new Error("Unknown expression: "+ndx);
 	}
 	
 	void compile(ASTNode ast) {
