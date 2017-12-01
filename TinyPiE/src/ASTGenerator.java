7645ae9975496f5b2c020331fd07f0520875f0d0
fjmtkut <190367v@ugs.kochi-tech.ac.jp>
2017-12-01 11:42:05 +0900

    演習２，３を追記

--- TinyPiE/src/ASTGenerator.java
+++ TinyPiE/src/ASTGenerator.java
@@ -6,12 +6,33 @@ import parser.TinyPiEParser.LiteralExprContext;
 import parser.TinyPiEParser.MulExprContext;
 import parser.TinyPiEParser.ParenExprContext;
 import parser.TinyPiEParser.VarExprContext;
+// ここから
+import parser.TinyPiEParser.AndExprContext;
+import parser.TinyPiEParser.OrExprContext;
+import parser.TinyPiEParser.NotExprContext;
+// ここまで
 
 public class ASTGenerator {	
 	ASTNode translateExpr(ParseTree ctxx) {
 		if (ctxx instanceof ExprContext) {
 			ExprContext ctx = (ExprContext) ctxx;
-			return translateExpr(ctx.addExpr());
+			return translateExpr(ctx.orExpr());
+// ここから演習２
+		} else if (ctxx instanceof OrExprContext) {
+			OrExprContext ctx = (OrExprContext) ctxx;
+			if (ctx.orExpr() == null)
+				return translateExpr(ctx.andExpr());
+			ASTNode lhs = translateExpr(ctx.orExpr());
+			ASTNode rhs = translateExpr(ctx.andExpr());
+			return new ASTBinaryExprNode(ctx.OROP().getText(), lhs, rhs);
+		} else if (ctxx instanceof AndExprContext) {
+			AndExprContext ctx = (AndExprContext) ctxx;
+			if (ctx.andExpr() == null)
+				return translateExpr(ctx.addExpr());
+			ASTNode lhs = translateExpr(ctx.andExpr());
+			ASTNode rhs = translateExpr(ctx.addExpr());
+			return new ASTBinaryExprNode(ctx.ANDOP().getText(), lhs, rhs);
+// ここまで
 		} else if (ctxx instanceof AddExprContext) {
 			AddExprContext ctx = (AddExprContext) ctxx;
 			if (ctx.addExpr() == null)
@@ -26,6 +47,21 @@ public class ASTGenerator {
 			ASTNode lhs = translateExpr(ctx.mulExpr());
 			ASTNode rhs = translateExpr(ctx.unaryExpr());
 			return new ASTBinaryExprNode(ctx.MULOP().getText(), lhs, rhs);
+// ここから演習３
+		} else if (ctxx instanceof NotExprContext) {
+			NotExprContext ctx = (NotExprContext) ctxx;
+			String operand = "";
+			if (ctx.VALUE() != null) {
+				operand = ctx.VALUE().getText();
+			} else if (ctx.IDENTIFIER() != null) {
+				operand = ctx.IDENTIFIER().getText();
+			}
+			if (ctx.SUBOP() != null) {
+				return new ASTUnaryExprNode(ctx.SUBOP().getText(), operand);
+			} else if (ctx.NOTOP() != null){
+				return new ASTUnaryExprNode(ctx.NOTOP().getText(), operand);
+			}
+// ここまで
 		} else if (ctxx instanceof LiteralExprContext) {
 			LiteralExprContext ctx = (LiteralExprContext) ctxx;
 			int value = Integer.parseInt(ctx.VALUE().getText());
