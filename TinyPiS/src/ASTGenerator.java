be202954ad3ee41b7c152d6b45aaf0ba464800c0
fjmtkut <190367v@ugs.kochi-tech.ac.jp>
2017-12-03 18:50:04 +0900

    演習７を追記

--- TinyPiS/src/ASTGenerator.java
+++ TinyPiS/src/ASTGenerator.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+//ここから
+import parser.TinyPiSParser.ProgContext;
+import parser.TinyPiSParser.StmtContext;
+import parser.TinyPiSParser.CompoundStmtContext;
+import parser.TinyPiSParser.AssignStmtContext;
+import parser.TinyPiSParser.IfStmtContext;
+import parser.TinyPiSParser.WhileStmtContext;
+import parser.TinyPiSParser.VarDeclsContext;
+//ここまで
 
 import parser.TinyPiSParser.AndExprContext;
 import parser.TinyPiSParser.OrExprContext;
@@ -12,7 +24,41 @@ import parser.TinyPiSParser.VarExprContext;
 
 public class ASTGenerator {	
 	ASTNode translate(ParseTree ctxx) {
-		if (ctxx instanceof ExprContext) {
+		if (ctxx instanceof ProgContext) {
+			ProgContext ctx = (ProgContext) ctxx;
+			ArrayList<String> varDecls = new ArrayList<String>();
+			for (TerminalNode token: ctx.varDecls().IDENTIFIER())
+				varDecls.add(token.getText());
+			ASTNode stmt = translate(ctx.stmt());
+			return new ASTProgNode(varDecls, stmt);
+		} else if (ctxx instanceof CompoundStmtContext) {
+			CompoundStmtContext ctx = (CompoundStmtContext) ctxx;
+			ArrayList<ASTNode> stmts = new ArrayList<ASTNode>();
+			for (StmtContext t: ctx.stmt()) {
+				ASTNode n = translate(t);
+				stmts.add(n);
+			}
+			return new ASTCompoundStmtNode(stmts);
+		} else if (ctxx instanceof AssignStmtContext) {
+			AssignStmtContext ctx = (AssignStmtContext) ctxx;
+			String var = ctx.IDENTIFIER().getText();
+			ASTNode expr = translate(ctx.expr());
+			return new ASTAssignStmtNode(var, expr);
+		} else if (ctxx instanceof IfStmtContext) {
+			IfStmtContext ctx = (IfStmtContext) ctxx;
+			ASTNode cond = translate(ctx.expr());
+			ASTNode thenClause = translate(ctx.stmt(0));
+			ASTNode elseClause = translate(ctx.stmt(1));
+			return new ASTIfStmtNode(cond, thenClause, elseClause);
+		} else if (ctxx instanceof WhileStmtContext) {
+			WhileStmtContext ctx = (WhileStmtContext) ctxx;
+			ASTNode cond = translate(ctx.expr());
+			ASTNode stmt = translate(ctx.stmt());
+			return new ASTWhileStmtNode(cond, stmt);
+		} else if (ctxx instanceof ParenExprContext) {
+			ParenExprContext ctx = (ParenExprContext) ctxx;
+			return translate(ctx.expr());
+		} else if (ctxx instanceof ExprContext) {
 			ExprContext ctx = (ExprContext) ctxx;
 			return translate(ctx.orExpr());
 		} else if (ctxx instanceof OrExprContext) {
